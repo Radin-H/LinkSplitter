7fefe5efa62ba72cff91a62aef0d348f45f5a1f1
Radin-H <radinhakimjavadi@gmail.com>
2020-03-18 21:15:19 -0400

    Fixed Radio Error

--- home.js
+++ home.js
@@ -4,17 +4,20 @@ function thething(){
     let list = document.getElementById('text1').value.split('\n');
     let out = document.getElementById('text2');
     let char = document.getElementById('split').value;
-    let z = document.getElementById('0').value;
-    let o = document.getElementById('1').value;
+    let left = document.getElementById('left');
+    let right = document.getElementById('right');
 
     var newlist = [];
     // console.log(list.length);
     for(var i = 0; list.length > i;i++){
         var e = list[i];
         if(e != null){
-            if(o == 0){
+            console.log('15');
+            if(right.checked){
+                console.log('rigjt');
             newlist.push(e.split(char)[1] + '');
-            } else{
+            } else if (left.checked){
+                console.log('left');
                 newlist.push(e.split(char)[0] + '');
             }
             // console.log(e.split('-')+ '');
